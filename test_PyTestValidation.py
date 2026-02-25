bdf603aa44a6ea8a2a2099b1e6e2668ff2d6bc03
Saumya Singh <singhsaumyait034@gmail.com>
2026-02-25 12:38:32 +0530

    PyTest Yield Fixture

--- test_PyTestValidation.py
+++ test_PyTestValidation.py
@@ -4,10 +4,18 @@ import pytest
 @pytest.fixture(scope="module")
 def preWork():
     print("I setup browser instance from module")
+    return "pass"
+
+@pytest.fixture(scope="function")
+def secondWork():
+    print("I setup browser instance from function")
+    yield # will execute you testcase and then below code will execute
+    print("I teardown browser instance from function")
 
 
-def test_initialCheck(preWork):
+def test_initialCheck(preWork, secondWork):
     print("This is the first Test")
+    assert preWork == "pass"
 
 def test_secondCheck(preSetupWork):
     print("This is the Second Test")
